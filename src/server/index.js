2fe17a7ec55b4c3339552f1f17cc7078d04ca574
Daniel Huisman <daniel@huisman.me>
2017-04-01 18:06:40 +0200

    Explicitly depend on chalk package

--- src/server/index.js
+++ src/server/index.js
@@ -10,3 +10,5 @@ const server = http.createServer(app.callback());
 server.listen(config.port, () => {
     logger.info(`Server started listening on port ${config.port}`);
 });
+
+export default server;
